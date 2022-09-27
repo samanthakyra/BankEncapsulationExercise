2c1f77b57dd13c6ecf101a2805be11ab8bb6ff8b
samanthakyra <samanthakyramendoza@gmail.com>
2022-09-27 16:22:03 -0700

    initial commit

---
+++ BankEncapsulationExercise/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace BankEncapsulationExercise
+{
+    public class BankAccount
+    {
+        private double _balance;
+        public void Deposit(double amount)
+
+        {
+            Console.WriteLine($"There is a deposit of {amount, 0:c}. It will be processed into your account shortly");
+            _balance = amount;
+        }
+        public double GetBalance()
+        {
+            return _balance;
+        }
+    }
+}
+
---
+++ BankEncapsulationExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BankEncapsulationExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount money = new BankAccount();
+
+            Console.WriteLine("How much would you like to deposit?");
+            double amountToDeposit = double.Parse(Console.ReadLine());
+
+            money.Deposit(amountToDeposit);
+            double userBalance = money.GetBalance();
+
+            Console.WriteLine($"Your current balance is: {userBalance,0:c}");
+        }
+    }
+}
+
